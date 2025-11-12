64a6b43f2e83dcbbcb9b16423701767a905810b5
Rohan Pandey <rohan.pandey-engineering@1mg.com>
2025-11-12 13:43:20 +0530

    testing the s3 bucket creation is success or not using localstack

--- backend.tf
+++ backend.tf
@@ -1,9 +1,13 @@
 terraform {
   backend "s3" {
-    bucket  = "willcloude-devops-bucket-test"
-    region  = "ap-southeast-1"
-    key     = "s3-github-actions/terraform.tfstate"
-    encrypt = true
+    bucket                      = "willcloud-devops-bucket-state"
+    key                         = "localstack/terraform.tfstate"
+    region                      = "ap-southeast-1"
+    endpoint                    = "http://localhost.localstack.cloud:4566"
+    access_key                  = "test"
+    secret_key                  = "test"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
   }
   required_version = ">=0.13.0"
   required_providers {
--- provider.tf
+++ provider.tf
@@ -1,11 +1,19 @@
 provider "aws" {
-  access_key                  = "test"
-  secret_key                  = "test"
-  region                      = "us-east-1"
-  s3_use_path_style           = true
+  access_key = "test"
+  secret_key = "test"
+  region     = "ap-southeast-1"
+
+  # Disable all AWS credential + metadata checks
   skip_credentials_validation = true
+  skip_metadata_api_check     = true
   skip_requesting_account_id  = true
+
+  s3_use_path_style = true
+
   endpoints {
-    s3 = "http://localhost.localstack.cloud:4566"
+    s3       = "http://localhost.localstack.cloud:4566"
+    dynamodb = "http://localhost.localstack.cloud:4566"
+    lambda   = "http://localhost.localstack.cloud:4566"
+    iam      = "http://localhost.localstack.cloud:4566"
   }
 }
