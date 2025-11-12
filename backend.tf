f7e2e52736890b12caee09c38b9d890e400ca2f2
Rohan Pandey <rohan.pandey-engineering@1mg.com>
2025-11-12 13:58:29 +0530

    testing the s3 bucket creation is success or not using localstack

--- backend.tf
+++ backend.tf
@@ -3,10 +3,12 @@ terraform {
     bucket                      = "terraform-backend-localstack"
     key                         = "state/terraform.tfstate"
     region                      = "us-east-1"
+    access_key                  = "test"
+    secret_key                  = "test"
     skip_credentials_validation = true
     skip_metadata_api_check     = true
     skip_requesting_account_id  = true
-    force_path_style            = true
+    use_path_style              = true
     endpoints = {
       s3 = "http://localhost.localstack.cloud:4566"
     }
