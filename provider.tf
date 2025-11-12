3a1517a7de1e18b80f58c6ffa2de7a94e7825ecf
Rohan Pandey <rohan.pandey-engineering@1mg.com>
2025-11-12 13:04:35 +0530

    in progress

---
+++ provider.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  access_key                  = "test"
+  secret_key                  = "test"
+  region                      = "us-east-1"
+  s3_use_path_style           = true
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  endpoints {
+    s3 = "http://localhost.localstack.cloud:4566"
+  }
+}
