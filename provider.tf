896da3ee68ee9f4871478a602f3841913e47b32f
Rohan Pandey <rohan.pandey-engineering@1mg.com>
2025-11-13 12:02:35 +0530

    testing the ec2 creation is success or not using localstack

--- provider.tf
+++ provider.tf
@@ -1,12 +1,16 @@
 provider "aws" {
-  region            = "us-east-1"
-  access_key        = "test"
-  secret_key        = "test"
+  region     = "us-east-1"
+  access_key = "test"
+  secret_key = "test"
+
   s3_use_path_style = true
 
   endpoints {
-    s3  = "http://localhost.localstack.cloud:4566"
-    ec2 = "http://localhost.localstack.cloud:4566"
-    iam = "http://localhost.localstack.cloud:4566"
+    s3         = "http://localhost.localstack.cloud:4566"
+    ec2        = "http://localhost.localstack.cloud:4566"
+    iam        = "http://localhost.localstack.cloud:4566"
+    sts        = "http://localhost.localstack.cloud:4566"
+    cloudwatch = "http://localhost.localstack.cloud:4566"
+    logs       = "http://localhost.localstack.cloud:4566"
   }
 }
