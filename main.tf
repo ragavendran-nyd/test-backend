0c032a701d5734ba6d6a9ba5ee98fd07b3a94f0f
Rohan Pandey <rohan.pandey-engineering@1mg.com>
2025-11-19 16:03:03 +0530

    added statelock file with dynamodb for smooth and safe deployments

--- main.tf
+++ main.tf
@@ -1,7 +1,21 @@
-resource "aws_s3_bucket" "backend_bucket" {
-  bucket = "willcloud-backend-s3"
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "willcloud-tf-state-bucket"
 }
 
-output "bucket_name" {
-  value = aws_s3_bucket.backend_bucket.bucket
+resource "aws_s3_bucket_versioning" "tf_state_versioning" {
+  bucket = aws_s3_bucket.tf_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "tf_lock" {
+  name         = "terraform-lock-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
