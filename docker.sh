5021036f0151589504a024e6af269e114ac3e531
Rohan Pandey <rohan.pandey-engineering@1mg.com>
2025-11-19 17:33:44 +0530

    spinning up an ec2 instance with keycloak + postgres using terraform

--- docker.sh
+++ docker.sh
@@ -8,15 +8,22 @@ amazon-linux-extras install docker -y
 systemctl start docker
 systemctl enable docker
 
-# Install Docker Compose
+# Install Docker Compose v2 (correct for Amazon Linux 2)
 curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 \
   -o /usr/local/bin/docker-compose
+
 chmod +x /usr/local/bin/docker-compose
 
-# Move compose file
+# Add symlink so Terraform remote-exec can find it in PATH
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose || true
+
+# Validate install
+docker-compose --version
+
+# Prepare app directory
 mkdir -p /opt/app
 mv /home/ec2-user/docker-compose.yml /opt/app/docker-compose.yml
 cd /opt/app
 
-# Start services
+# Start containers
 docker-compose up -d
