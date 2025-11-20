791ce0027e52886c3f2ef1839d52533129c3245f
Rohan Pandey <rohan.pandey-engineering@1mg.com>
2025-11-20 17:14:28 +0530

    spinning up an ec2 instance with keycloak + postgres using terraform

--- docker.sh
+++ docker.sh
@@ -1,29 +1,37 @@
 #!/bin/bash
 set -e
 
+### Update and install Docker ###
 yum update -y
-
-# Install Docker
 amazon-linux-extras install docker -y
+
 systemctl start docker
 systemctl enable docker
 
-# Install Docker Compose v2 (correct for Amazon Linux 2)
+### Install Docker Compose v2 ###
 curl -SL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-x86_64 \
   -o /usr/local/bin/docker-compose
-
 chmod +x /usr/local/bin/docker-compose
-
-# Add symlink so Terraform remote-exec can find it in PATH
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose || true
 
-# Validate install
+echo "Docker Compose installed:"
 docker-compose --version
 
-# Prepare app directory
+### Prepare application folder ###
 mkdir -p /opt/app
+
+# Move files placed by Terraform
 mv /home/ec2-user/docker-compose.yml /opt/app/docker-compose.yml
+mv /home/ec2-user/disable-ssl.sh /opt/app/disable-ssl.sh
+
+chmod +x /opt/app/disable-ssl.sh
+
 cd /opt/app
 
-# Start containers
+### Pull images first (avoids timeout) ###
+docker-compose pull
+
+### Start stack ###
 docker-compose up -d
+
+echo "Docker stack started."
