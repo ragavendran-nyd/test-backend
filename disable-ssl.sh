e85ebba78f00e86f963bc9ff7c6c8c2808a259e6
Rohan Pandey <rohan.pandey-engineering@1mg.com>
2025-11-24 23:11:53 +0530

    pushing to run the pipeline

--- disable-ssl.sh
+++ disable-ssl.sh
@@ -1,15 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -e
-
-# expects KEYCLOAK_ADMIN and KEYCLOAK_ADMIN_PASSWORD to be available in environment
-SERVER_URL="http://localhost:8080"
-
-# try login until succeeds
-until /opt/keycloak/bin/kcadm.sh config credentials --server ${SERVER_URL} --realm master --user ${KEYCLOAK_ADMIN} --password ${KEYCLOAK_ADMIN_PASSWORD} >/dev/null 2>&1; do
-  sleep 2
-done
-
-# set sslRequired to NONE
+# intended to run inside a Keycloak image (or be executed from keycloak-fix)
+SERVER="http://keycloak:8080"
+echo "waiting for Keycloak admin endpoint..."
+until curl -sSf "${SERVER}/realms/master" >/dev/null 2>&1; do sleep 2; done
+/opt/keycloak/bin/kcadm.sh config credentials --server "${SERVER}" --realm master --user "${KEYCLOAK_ADMIN:-admin}" --password "${KEYCLOAK_ADMIN_PASSWORD:-admin}"
 /opt/keycloak/bin/kcadm.sh update realms/master -s sslRequired=NONE || true
-
-echo "sslRequired set to NONE"
+echo "sslRequired disabled"
